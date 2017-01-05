e3a00283b59ddd4041a486ab77d41fa123357ed2
YOU <you@YOUdeMac-mini.local>
2017-01-05 17:20:43 +0800

    Initial Commit

---
+++ mainTabView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mainTabView
+//
+//  Created by YOU on 2017/1/5.
+//  Copyright © 2017年 wws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mainTabView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mainTabView
+//
+//  Created by YOU on 2017/1/5.
+//  Copyright © 2017年 wws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
